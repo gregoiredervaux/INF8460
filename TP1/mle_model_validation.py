baebe6a8571627c1934e9f6ab65b34a7904b5699
Nico <nicolas.golenvaux@student.uclouvain.be>
2019-09-08 00:28:35 -0400

    premier essai pour mle validation (le modèle nltk ne fonctionne pas correctement)

--- TP1/mle_model_validation.py
+++ TP1/mle_model_validation.py
@@ -13,9 +13,12 @@ et une variable vocabulary de type Vocabulary.
 On peut ensuite entraîner le modèle avec la méthode model.fit(ngrams). Attention, la documentation prête à confusion :
 la méthode attends une liste de liste de n-grammes (`list(list(tuple(str)))` et non pas `list(list(str))`).
 """
+import nltk
 from nltk.lm.models import MLE
 from nltk.lm.vocabulary import Vocabulary
 from nltk.lm.preprocessing import padded_everygram_pipeline
+import mle_ngram_model as mle
+import preprocess_corpus as pre
 
 
 def train_MLE_model(corpus, n):
@@ -26,7 +29,7 @@ def train_MLE_model(corpus, n):
     :param n: l'ordre du modèle
     :return: un modèle entraîné
     """
-    pass
+    return mle.NgramModel(corpus, n)
 
 
 def compare_models(your_model, nltk_model, corpus, n):
@@ -42,7 +45,17 @@ def compare_models(your_model, nltk_model, corpus, n):
     :param corpus: list(list(str)), une liste de phrases tokenizées à tester
     :return: float, la proportion de n-grammes incorrects
     """
-    pass
+    n_gram_count = 0
+    n_gram_diff = 0
+    counts = your_model.counts
+    for n_gram_begining in counts:
+        for n_gram_end in counts[n_gram_begining]:
+            if(counts[n_gram_begining][n_gram_end] != nltk_model.unmasked_score(n_gram_end, n_gram_begining)):
+                print("Différente probabilité pour le n_gram " + str(n_gram_begining) +" " +str(n_gram_end))
+                print(str(counts[n_gram_begining][n_gram_end]) + " vs " + str(nltk_model.unmasked_score(n_gram_end, n_gram_begining)))
+                n_gram_diff += 1
+            n_gram_count += 1
+    return n_gram_diff/n_gram_count
 
 
 if __name__ == "__main__":
@@ -52,4 +65,11 @@ if __name__ == "__main__":
     `compare_models `pour vérifier qu'ils donnent les mêmes résultats. 
     Comme corpus de test, vous choisirez aléatoirement 50 phrases dans `shakespeare_train`.
     """
-    pass
+
+    fileName = "shakespeare_train"
+    corpus = pre.read_and_preprocess("./data/" + fileName + ".txt")
+    n = 1
+    other_model = nltk.lm.models.MLE(n, corpus)
+    my_model = train_MLE_model(corpus, n)
+    diff = compare_models(my_model,other_model,corpus,n)
+    print(diff)
