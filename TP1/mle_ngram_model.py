46e63a500ac5e55c6461aaf8f1080209c6cd314d
Coralie <coralie.verrier@etu.utc.fr>
2019-09-21 19:20:39 -0400

    K ngrams

--- TP1/mle_ngram_model.py
+++ TP1/mle_ngram_model.py
@@ -13,6 +13,8 @@ from itertools import permutations
 from random import choices
 import collections
 from nltk.lm.preprocessing import pad_both_ends
+import operator
+import collections
 
 import preprocess_corpus as pre
 
@@ -212,3 +214,20 @@ if __name__ == "__main__":
         print("\nn=" + str(n))
         for tuple_n in contexts[n]:
             print("%s --> %s" % (str(tuple_n), str(Model.predict_next(tuple_n))))
+
+    print("--------------")
+
+    for i in range(1,4):
+        print("\n--- Pour n = " + str(i) + "---")
+        counts = count_ngrams(corpus, i)
+        newDict = {}
+
+        for key in counts:
+            for k,v in counts[key].items():
+                newDict[key + tuple([k])] = v
+
+        sorted_dict = sorted(newDict.items(), key=operator.itemgetter(1), reverse=True)
+        count_dict = collections.OrderedDict(sorted_dict)
+
+        for key in list(count_dict)[:20]:
+            print(str(key) + " : " + str(count_dict[key]))
