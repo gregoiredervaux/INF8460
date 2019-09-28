61908120c9a399e95faf2181a18ddcffdc60f72b
gregoire <gregoire.dervaux@2019.icam.fr>
2019-09-28 13:52:06 -0400

    question 3 et 4, a vérifier car resultats bizarres

--- TP2/recap.py
+++ TP2/recap.py
@@ -2,7 +2,11 @@ import os
 import re
 import nltk
 import collections
+import math
+import itertools
+import time
 import numpy as np
+from scipy.spatial import distance
 from nltk.corpus import stopwords
 nltk.download('stopwords')
 
@@ -20,7 +24,7 @@ dictionnaire = {
 nb_failed_files = 0
 for train_type in ["test", "train"]:
     for classification in ["pos", "neg"]:
-        path = './data/' + train_type + '/' + classification + '/'
+        path = './fake_data/' + train_type + '/' + classification + '/'
         for file in os.listdir(path):
             id_review, rate_review = file.split("_")
             rate_review = rate_review.split(".txt")[0]
@@ -70,7 +74,7 @@ def build_voc(dico_train):
                 else:
                     count[word] = 1
     n = 0
-    f = open("./data/vocab.txt", "w+")
+    f = open("./fake_data/vocab.txt", "w+")
     for word in count:
         if count[word] >= 5:
             n += 1
@@ -126,14 +130,142 @@ print(get_top_unigrams_per_cls(10, "neg"))
 ##################### Question 2 #####################
 
 unigrams = get_top_unigrams(5000)
+print("les 5000 premiers unigrams")
 print(unigrams[0:30])
-BoW = []
+debut_mm = time.time()
+BoW = np.array([])
+for train_type in ["test", "train"]:
+    for classification in ["pos", "neg"]:
+        # on construit un petit BoW, pur le type et la classe des documents
+        # pour eviter les appends a chaque nouvelle ligne et profiter des types numpy
+        BoW_part = np.zeros((len(dictionnaire[train_type][classification]), len(unigrams)))
+        for index, review in enumerate(dictionnaire[train_type][classification]):
+            for word in dictionnaire[train_type][classification][review]['review']:
+                if word in unigrams:
+                    BoW_part[index, unigrams.index(word)] += 1
+
+        BoW = np.append(BoW, BoW_part, axis=0) if BoW.size != 0 else BoW_part
+
+print("\nBag of Word")
+print("fait en : " + str(time.time() - debut_mm) + "s")
+print(BoW)
+
+debut_mm = time.time()
+TFIDF_BoW = BoW.copy()
+# liste des idf par index (cf unigrams)
+idf = []
+for i,_ in enumerate(unigrams):
+    dfi = 0
+    for j in range(TFIDF_BoW.shape[0]):
+        # on compte le nombre de documents qui contiennent ce mot
+        dfi += 1 if TFIDF_BoW[j, i] != 0 else 0
+    # on calcul et ajoute le idf a la liste des idf
+    idf.append(math.log(TFIDF_BoW.shape[0] / dfi))
+
+# on modifie la matrice
+for j in range(TFIDF_BoW.shape[0]):
+    for i, _ in enumerate(unigrams):
+        value = TFIDF_BoW[j, i] * idf[i]
+        TFIDF_BoW[j, i] = value
+print("\nBag of Word TFIDF")
+print("fait en : " + str(time.time() - debut_mm) + "s")
+print(TFIDF_BoW)
+
+
+debut_mm = time.time()
+matrice_MM = np.zeros((len(unigrams), len(unigrams)))
 for train_type in ["test", "train"]:
     for classification in ["pos", "neg"]:
         for review in dictionnaire[train_type][classification]:
-            new_ligne = np.zeros(len(unigrams))
-            for word in dictionnaire[train_type][classification][review]['review']:
-                if word in unigrams:
-                    new_ligne[unigrams.index(word)] = 1
-            BoW.append(new_ligne)
+            # on selectionne seulement les mots qui font parti des 5000 selectionnés
+            words_to_consider  = list(set(unigrams).intersection(dictionnaire[train_type][classification][review]['review']))
+            index_to_consider = [unigrams.index(couple_word) for couple_word in words_to_consider]
+            permutation = itertools.permutations(index_to_consider, 2)
+            for index_to_consider in permutation:
+                matrice_MM[index_to_consider] += 1
+
+print("matrice_MM")
+print("fait en : " + str(time.time() - debut_mm) + "s")
+print(matrice_MM)
+
+debut_mm = time.time()
+def calculate_PPMI(matrice_MM):
+    colsum = np.sum(matrice_MM, axis=0)
+    row_sum = np.sum(matrice_MM, axis=1)
+    sum = np.sum(matrice_MM)
+    expected = np.zeros((matrice_MM.shape[0], matrice_MM.shape[1]))
+    ppmi =  np.zeros((matrice_MM.shape[0], matrice_MM.shape[1]))
+    for index,_ in np.ndenumerate(expected):
+        expected[index] = (row_sum[index[0]] * colsum[index[1]]) / sum
+        pmi = math.log(matrice_MM[index] / expected[index]) if matrice_MM[index] > 0 else 0
+        ppmi[index] = pmi if pmi > 0 else 0
+
+    return ppmi
+
+print("matrice_PPMI")
+matrice_PPMI = calculate_PPMI(matrice_MM)
+print("fait en : " + str(time.time() - debut_mm) + "s")
+print(matrice_PPMI)
+
+##################### Question 3 #####################
+
+def get_euclidean_distance(v1 ,v2):
+    return distance.euclidean(v1, v2)
+
+def get_cosinus_distance(v1, v2):
+    return distance.cosine(v1, v2)
+
+def get_most_similar_PPMI(word, metric, n):
+    index_word = unigrams.index(word)
+    ligne_word = matrice_PPMI[index_word]
+    vector_distance = {}
+    i = 0
+    if metric == "cosinus":
+        for index, ligne in enumerate(matrice_PPMI):
+            vector_distance[unigrams[index]] = get_cosinus_distance(ligne_word, ligne)
+            i += 1
+    elif metric == "euclidean":
+        for index, ligne in enumerate(matrice_PPMI):
+            vector_distance[unigrams[index]] = get_euclidean_distance(ligne_word, ligne)
+            i += 1
+    else:
+        print("metric inconnue")
+
+    counter = collections.Counter(vector_distance)
+    top_unigrams = counter.most_common(n)
+    return [couple[0] for couple in top_unigrams]
+
+debut_mm = time.time()
+print("\nles mots les plus proches de \"bad\": ")
+most_similar = get_most_similar_PPMI("bad", "euclidean", 5)
+print(most_similar)
+print("fait en : " + str(time.time() - debut_mm) + "s")
+
+
+def get_most_similar_TFIDF(word, metric, n):
+    index_word = unigrams.index(word)
+    column_word = TFIDF_BoW[:, index_word]
+    vector_distance = {}
+    if metric == "cosinus":
+        for index, column in enumerate(TFIDF_BoW.T):
+            vector_distance[unigrams[index]] = get_cosinus_distance(column_word, column)
+    elif metric == "euclidean":
+        for index, column in enumerate(TFIDF_BoW.T):
+            vector_distance[unigrams[index]] = get_euclidean_distance(column_word, column)
+    else:
+        print("metric inconnue")
+
+    counter = collections.Counter(vector_distance)
+    top_unigrams = counter.most_common(n)
+    return [couple[0] for couple in top_unigrams]
+
+debut_mm = time.time()
+print("les mots les plus proches de \"bad\": ")
+most_similar = get_most_similar_TFIDF("bad", "euclidean", 5)
+print(most_similar)
+print("fait en : " + str(time.time() - debut_mm) + "s")
+
+
+
+
 ## Pour la dernière question, peut être essayer une réduction de dimension
